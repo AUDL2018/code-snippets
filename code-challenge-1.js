73da68e63935a2292589a8bdb476ad08b2fe1e69
Henning Horn <hh@henninghorn.dk>
2018-03-15 18:05:38 +0100

    Create code-challenge-1.js

---
+++ code-challenge-1.js
@@ -0,0 +1,19 @@
+/** 🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅
+ * 🏅🏅🏅 QUICK CODE CHALLENGE 🏅🏅🏅
+ * 🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅🏅
+ */
+
+/**
+ * 😎 Create a BMI calculator
+ *
+ * 🤔 BMI is calculated by: weight / (height * height)
+ * 🤔 Too low (< 18.5), Normal (18.5 - 25), Overweight (> 25)
+ *
+ * 👉 Add an input field for weight and height
+ * 👉 When a user submits the form, calculate the BMI based on the values
+ * 👉 Show the calculated result on page
+ * 👉 Also! Display a message saying if it is a good a bad BMI
+ * 
+ * 🙌 First student or group to create a working calculator gets a prize! 🙌
+ * 
+ */
